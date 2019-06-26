a5b60ff0d72e0e7022ae8a214a4a2f32b48aa8bb
Justin Kerntz and Katlin Anderson <katlin@>
2019-06-26 09:36:23 -0700

    add editing function to Sheets

--- models/Sheet.ts
+++ models/Sheet.ts
@@ -4,5 +4,13 @@ export class Sheet {
     
     constructor() {}
 
+    writeSomething(newWriting: string) {
+        this.writing = newWriting;
+        this.drawingUrl = null;
+    }
 
+    drawSomething(newDrawingUrl: string) {
+        this.writing = null;
+        this.drawingUrl = newDrawingUrl;
+    }
 }
